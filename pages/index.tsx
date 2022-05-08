89bc4d5fdfc62c681d9ad5323e2a0ebb285ee625
Baptiste Dauphouy <bdauphouy@gmail.com>
2022-05-09 00:34:16 +0200

    feat: initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>
+          Atabul&apos;app - Offres et avantages pour les professionnels de la
+          restauration et de l&apos;hôtellerie
+        </title>
+        <meta
+          name="description"
+          content="Atabul'app est une application qui a pour but de permettre des professionnelles de l’hôtellerie et de la restauration de profiter des avantages et des offres à d'autres pros du milieu (les étudiants, les brigades etc)."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Atabul&apos;app</h1>
+    </>
+  )
+}
+
+export default Home
