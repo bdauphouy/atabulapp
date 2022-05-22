1ba1ebf751c7ad2b8b09f32c842b43466713bf5f
Baptiste Dauphouy <bdauphouy@gmail.com>
2022-05-22 20:45:43 +0200

    feat: nextjs document

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="fr">
+      <Head>
+        <title>
+          Atabulapp - Offres et avantages pour les professionnels de la
+          restauration et de l&apos;hôtellerie
+        </title>
+        <meta
+          name="description"
+          content="Atabul'app est une application qui a pour but de permettre des professionnelles de l’hôtellerie et de la restauration de profiter des avantages et des offres à d'autres pros du milieu (les étudiants, les brigades etc)."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,9 @@
-import Head from 'next/head'
-import Footer from '@/components/shared/Footer'
-
 const Home = () => {
   return (
-    <>
-      <Head>
-        <title>
-          Atabul&apos;app - Offres et avantages pour les professionnels de la
-          restauration et de l&apos;hôtellerie
-        </title>
-        <meta
-          name="description"
-          content="Atabul'app est une application qui a pour but de permettre des professionnelles de l’hôtellerie et de la restauration de profiter des avantages et des offres à d'autres pros du milieu (les étudiants, les brigades etc)."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Footer />
-    </>
+    <div className="flex h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-2xl font-medium">Atabulapp</h1>
+      <h2 className="text-gray">En cours de développement...</h2>
+    </div>
   )
 }
 
